38c1f18517f5f9a18bf80964ea200f02f7281049
gvernon2 <greg.j.vernon@gmail.com>
2022-12-09 00:21:44 -0700

    chore: cleanup runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,12 @@ using BenchmarkTools
 using Test
 import Statistics 
 
-@testset "JuliaSplineFEM.jl" begin
-    include( "../src/JuliaSplineFEM.jl" )
+include( "../src/JuliaSplineFEM.jl" )
+
+@testset "Basis" begin
     include( "test_Basis.jl" )
+end
+
+@testset "Quadrature" begin
     include( "test_Quadrature.jl" )
 end
